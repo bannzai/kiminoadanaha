ea154f1fe6d9074a841cbc5986bbd9b6da4ed87c
bannzai <kingkong999yhirose@gmail.com>
2022-12-13 21:19:11 +0900

    comment out debug

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -10,6 +10,5 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
-  debug: process.env.NODE_ENV === "development",
-  
+  //   debug: process.env.NODE_ENV === "development",
 });
