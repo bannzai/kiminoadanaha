2aa24fd776efebc911c904d1c09fc2225a536b8d
bannzai <kingkong999yhirose@gmail.com>
2022-12-14 08:50:09 +0900

    Add field of userID and accessTokenSecret

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,13 +4,17 @@ import { DefaultJWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
+      userID?: string;
       accessToken?: string;
+      accessTokenSecret?: string;
     } & DefaultSession["user"];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT extends Record<string, unknown>, DefaultJWT {
+    userID?: string;
     accessToken?: string;
+    accessTokenSecret?: string;
   }
 }
