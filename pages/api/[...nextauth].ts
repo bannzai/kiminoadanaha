3b24d08a32aa4c0b8d186d0668305ac8f3887d9a
bannzai <kingkong999yhirose@gmail.com>
2022-12-13 19:34:44 +0900

    with version 2.0

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -6,6 +6,8 @@ export default NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      version: "2.0", // opt-in to Twitter OAuth 2.0
     }),
   ],
+  secret: process.env.TWITTER_API_KEY_SECRET!,
 });
