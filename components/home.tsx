7b1ba83d430f125b45f59211e62d772aabfb031b
bannzai <kingkong999yhirose@gmail.com>
2022-12-14 08:48:19 +0900

    fix home

--- components/home.tsx
+++ components/home.tsx
@@ -1,6 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import {
   Button,
   Textarea,
@@ -10,10 +7,15 @@ import {
   AlertTitle,
   AlertDescription,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NameResponse } from "../pages/api/name";
+import { TweetResponse } from "../pages/api/tweets/[userID]";
+import { getSession, useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
+  const { data: session } = useSession({ required: true });
+  console.log({ session });
   const [tweet, setTweet] = useState("");
   const [named, setNamed] = useState<Extract<
     NameResponse,
@@ -24,13 +26,13 @@ export default function Home() {
     { result: "failure" }
   > | null>(null);
   const [loading, setLoading] = useState(false);
+  const [tweets, setTweets] = useState<string[]>([]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const inputValue = e.target.value;
-    setTweet(inputValue);
-  };
+  useEffect(() => {
+    if (tweets.length === 0) {
+      return;
+    }
 
-  const handleOnClick = () => {
     const f = async () => {
       setLoading(true);
       const response = await fetch("/api/name", {
@@ -38,8 +40,9 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ tweet: tweet }),
+        body: JSON.stringify({ tweets }),
       });
+
       const named = await (response.json() as Promise<NameResponse>);
       if (named.result === "success") {
         setNamed(named);
@@ -50,7 +53,35 @@ export default function Home() {
     };
 
     f();
+  }, [tweets]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
+    setTweet(inputValue);
   };
+
+  const handleOnClick = () => {
+    const f = async () => {
+      setLoading(true);
+      const response = await fetch(`/api/tweets/${session?.user.userID}`, {
+        method: "GET",
+        headers: {
+          "x-twitter-access-token": session?.user.accessToken!,
+          "x-twitter-access-token-secret": session?.user.accessTokenSecret!,
+        },
+      });
+      const tweets = await (response.json() as Promise<TweetResponse>);
+      if (tweets.result === "success") {
+        console.log({ res: tweets.response ?? "abc" });
+        setTweets(tweets.response);
+      } else {
+        setError(tweets);
+      }
+      setLoading(false);
+    };
+    f();
+  };
+
   return (
     <>
       <Text mb="8px">Tweet</Text>
